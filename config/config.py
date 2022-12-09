30bfdcbc5888ec0d0af9db41788aa4be985fddd9
star-boi-opksb <120212131+star-boi-opksb@users.noreply.github.com>
2022-12-10 00:13:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -32,7 +32,7 @@ OWNER_ID = list(
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Starboihackss723/op",
+    "https://github.com/star-boi-opksb/oosnd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
